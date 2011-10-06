1b551e4605caac6cfc968e5825c0fdf4ea8ecfc5
Josh Butts <josh@joshbutts.com>
2011-10-06 18:08:43 -0500

    update class 2

---
+++ class-2/homework.php
@@ -0,0 +1,35 @@
+<?php
+$strings = array("Austin is the capital of Texas.",
+                 "There are 50 states in the United States of America",
+                 "There is no better joke about muffins and an oven than this one:",
+                 "There are two muffins are in an oven...",
+                 "One says to the other: God it is hot in here",
+                 "The other one replies: Oh no... It's a talking muffin");
+
+
+// #1
+// calculate and print the number of alphanumeric characters in $strings
+
+// #2
+// calculate and print out how many words are in $strings
+
+// #3
+// calculate and print out how many spaces are contained in $strings
+
+// #4
+// calculate and print out how many capital letters are contained in $strings
+
+
+// #5
+// calculate and print how many times the word "the" appears in $strings
+
+
+// #6 -- Super Bonus
+// print out a table of information about the words contained in $strings. the table should have 2 columns,
+// which contain the word, and how many times the word appears in $strings.
+//
+// Example:
+// | Austin  | 1 |
+// | talking | 1 |
+// | muffins | 2 |
+
